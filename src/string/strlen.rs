88bd1e8588b9288c308322c3c4281f1774ce8237
York Xiang <bombless@126.com>
2016-06-13 19:10:18 +0800

    `strlen`: get rid of inferred casts

--- src/string/strlen.rs
+++ src/string/strlen.rs
@@ -4,18 +4,24 @@ use c_types::*;
 
 #[no_mangle]
 pub unsafe extern "C" fn strlen(mut s: *const c_schar) -> size_t {
+    
     #[inline]
     fn align() -> usize { core::mem::size_of::<size_t>() }
+    
     #[inline]
     fn ones() -> usize { !0usize }
+    
     #[inline]
     fn highs() -> usize { usize::wrapping_mul(ones(), usize::MAX)/2 + 1 }
+    
     #[inline]
     fn has_zero(x: usize) -> bool {
         usize::wrapping_sub(x, ones() & !x & highs()) != 0
     }
+    
+    // keep original address
     let a = s;
-    let mut w: *const size_t;
+    
     // check zero until align boundary
     while s as usize % align() != 0 {
         if *s == 0 {
@@ -23,15 +29,18 @@ pub unsafe extern "C" fn strlen(mut s: *const c_schar) -> size_t {
         }
         s = s.offset(1)
     }
+    
     // check with word-chunks
-    w = s as _;
+    let mut w = s as *const size_t;
     while !has_zero(*w) {
         w = w.offset(1)
     }
+    
     // find exact length
-    s = w as _;
+    s = w as *const c_schar;
     while *s != 0 {
         s = s.offset(1)
     }
+    
     s as usize - a as usize
 }
