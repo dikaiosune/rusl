51e633c7e1ef150d6fbfe98561fd08936b7b4f93
York Xiang <bombless@126.com>
2016-06-13 19:04:58 +0800

    `strlen`: turn macros into inlined functions

--- src/string/strlen.rs
+++ src/string/strlen.rs
@@ -4,19 +4,20 @@ use c_types::*;
 
 #[no_mangle]
 pub unsafe extern "C" fn strlen(mut s: *const c_schar) -> size_t {
-    macro_rules! align { () => { core::mem::size_of::<size_t>() } }
-    macro_rules! ones { () => { !0usize } }
-    macro_rules! highs { () => { usize::wrapping_mul(ones!(), usize::MAX)/2 + 1 } }
-    macro_rules! has_zero { ($x:expr) => {{
-        let x: usize = $x;
-        let ones: usize = ones!();
-        let highs: usize = highs!();
-        usize::wrapping_sub(x, ones & !x & highs) != 0
-    }} }
+    #[inline]
+    fn align() -> usize { core::mem::size_of::<size_t>() }
+    #[inline]
+    fn ones() -> usize { !0usize }
+    #[inline]
+    fn highs() -> usize { usize::wrapping_mul(ones(), usize::MAX)/2 + 1 }
+    #[inline]
+    fn has_zero(x: usize) -> bool {
+        usize::wrapping_sub(x, ones() & !x & highs()) != 0
+    }
     let a = s;
     let mut w: *const size_t;
     // check zero until align boundary
-    while s as usize % align!() != 0 {
+    while s as usize % align() != 0 {
         if *s == 0 {
             return s as usize - a as usize
         }
@@ -24,7 +25,7 @@ pub unsafe extern "C" fn strlen(mut s: *const c_schar) -> size_t {
     }
     // check with word-chunks
     w = s as _;
-    while !has_zero!(*w) {
+    while !has_zero(*w) {
         w = w.offset(1)
     }
     // find exact length
