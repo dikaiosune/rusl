f364ea90cbced54d90cb11793cd87cf66e789545
A.J. Gardner <aaron.j.gardner@gmail.com>
2017-02-09 22:42:22 -0600

    Add comments and links to musl changesets

--- src/exit/_Exit.rs
+++ src/exit/_Exit.rs
@@ -6,5 +6,10 @@ pub extern "C" fn _Exit(ec: c_int) -> ! {
         syscall!(EXIT_GROUP, ec);
         syscall!(EXIT, ec);
     }
+    // The loop, while technically unreachable, ensures this function
+    // is divergent. The syscall above returns (), so this code
+    // wouldn't typecheck if we didn't have this infinite loop. See
+    // this musl changeset for similar discussion:
+    // http://git.musl-libc.org/cgit/musl/commit/src/exit/_Exit.c?id=0c05bd3a9c165cf2f0b9d6fa23a1f96532ddcdb3
     loop {}
 }
